8e46bd48752b72ca41388b743b7449b26e779818
eepyemy <emy.ict@proton.me>
2025-06-17 18:08:55 +0200

    fixed a bit

--- backtrack.py
+++ backtrack.py
@@ -1,8 +1,16 @@
 from rich import print
+import curses
 import sys
 
+SCR = curses.initscr()
 def print_grid(grid):
-	print('\n'.join([' '.join([str(cell) for cell in row]) for row in grid]))
+	print('\n'.join([' '.join([str(cell) for cell in row]) for row in grid]), end="\r\r\r\r", flush=True)
+
+def print_in_place(grid):
+	for ind, row in enumerate(grid):
+		a = ' '.join([str(cell) for cell in row])
+		SCR.addstr(ind, 0, a)
+	SCR.refresh()
 
 def get_column(col, board):
 	return [x[col] for x in board]
@@ -24,62 +32,129 @@ def check_array(visible_amount, array, reverse):
 			saw_zero = True
 	return counter_seen == visible_amount if not saw_zero else counter_seen <= visible_amount
 
-def can_place(row, col, board, borders, value):
+def can_place(row, col, board, frame, value):
 	h = list(get_row(row, board))
 	v = list(get_column(col, board))
 	if value in h or value in v:
 		return False
 	h[col] = value
 	v[row] = value
-	if not check_array(borders["left"][row], h, False):
+	if not check_array(frame["left"][row], h, False):
 		return False
-	if not check_array(borders["right"][row], h, True):
+	if not check_array(frame["right"][row], h, True):
 		return False
-	if not check_array(borders["top"][col], v, False):
+	if not check_array(frame["top"][col], v, False):
 		return False
-	if not check_array(borders["bottom"][col], v, True):
+	if not check_array(frame["bottom"][col], v, True):
 		return False
 	return True
+
+def prefill(board, frame):
+	global FILLED_CELLS
+	for i in range(N):
+		if frame["top"][i] == 1:
+				board[0][i] = N
+		if frame["bottom"][i] == 1:
+				board[N-1][i] = N
+		if frame["left"][i] == 1:
+				board[i][0] = N
+		if frame["right"][i] == 1:
+				board[i][N-1] = N
+		for j in range(N):
+			if frame["top"][i] == N:
+				board[j][i] = j+1			
+			if frame["bottom"][i] == N:
+				board[N-1-j][i] = j+1
+			if frame["left"][i] == N:
+				board[i][j] = j+1
+			if frame["right"][i] == N:
+				board[i][N-1-j] = j+1
 
 def	found_solution(board):
 	global FOUND
 	if not FOUND:
-		FOUND = board[N-1][N-1] != 0
+		FOUND = FILLED_CELLS == N*N
 	return FOUND
 
-def backtrack(row, col, board, borders):
+def backtrack(row, col, board, frame):
+	global FILLED_CELLS, ATTEMPTS
+	prefilled = False
+	#if ATTEMPTS % 200 == 0:
+		#print(f"\n still solving, ... {ATTEMPTS} attempts")
+		#print_grid(board)
+	print_in_place(board)
 	if (found_solution(board)):
 		return
-	for choice in range(1, N+1):
-		if (can_place(row, col, board, borders, choice)):
-			board[row][col] = choice
-		else:
-			continue
-		if col == N-1 and row == N-1:
-			break
+	if (board[row][col] != 0):
+		prefilled = True
 		next_col = (col + 1) % N
 		next_row = row
 		if (next_col == 0):
 			next_row = (row + 1) % N
-		backtrack(next_row, next_col, board, borders)
-	if not found_solution(board):
-		board[row][col] = 0
-
+		backtrack(next_row, next_col, board, frame)
+	if not prefilled:
+		ATTEMPTS += 4
+		for choice in range(1, N+1):
+			if (can_place(row, col, board, frame, choice)):
+				board[row][col] = choice
+				FILLED_CELLS += 1
+			else:
+				continue
+			if col == N-1 and row == N-1:
+				break
+			next_col = (col + 1) % N
+			next_row = row
+			if (next_col == 0):
+				next_row = (row + 1) % N
+			backtrack(next_row, next_col, board, frame)
+			if not found_solution(board):
+				board[row][col] = 0
+				FILLED_CELLS -= 1
+def count_filled(board):
+	global FILLED_CELLS
+	for row in board:
+		for cell in row:
+			if cell != 0:
+				FILLED_CELLS += 1
 FOUND = False
-N = 4
+ATTEMPTS = 0
+N = int(sys.argv[1])
 positions = ["top", "bottom", "left", "right"]
-# borders = "1 2 2 3 2 3 2 1 1 2 3 2 3 2 2 1".split(" ")
+frame = sys.argv[2].split(" ") #"1 2 2 4 4 2 2 1 1 2 2 4 4 2 2 1".split(" ") 
 # 4 2 3 1
 # 2 4 1 3
 # 1 3 4 2
 # 3 1 2 4
 
-borders = "1 2 2 3 3 2 1 2 1 3 2 3 2 1 3 2".split(" ")
+# recurions depth reached
+# 1 2 2 4 4 2 2 1 1 2 2 4 4 2 2 1
+# 4 3 2 1
+# 3 1 4 2
+# 2 4 1 3
+# 1 2 3 4
 
-borders = {name:[int(borders[x*N + y]) for y in range(N)] for x, name in enumerate(positions)}
+# frame = "1 2 2 3 3 2 1 2 1 3 2 3 2 1 3 2".split(" ")
+
+# 8 4 4 3 3 2 2 1 1 2 2 3 3 5 4 8 8 5 4 3 3 2 2 1 1 2 2 3 3 5 4 8
+# 1 2 3 4 5 6 7 8
+# 2 4 5 3 6 1 8 7
+# 3 1 4 7 2 8 5 6
+# 4 3 7 8 1 2 6 5
+# 5 6 2 1 8 7 3 4
+# 6 8 1 2 7 5 4 3
+# 7 5 8 6 3 4 1 2
+# 8 7 6 5 4 3 2 1
+
+FILLED_CELLS = 0
+frame = {name:[int(frame[x*N + y]) for y in range(N)] for x, name in enumerate(positions)}
 board = [[0 for x in range(N)] for x in range(N)]
-print(board)
-
-print(borders)
-backtrack(0, 0, board, borders)
+print_grid(board)
+print(frame)
+print("\nprefilling board:")
+prefill(board, frame)
+count_filled(board)
+print_grid(board)
+backtrack(0, 0, board, frame)
+print("\nresult:")
+print(f"\n attempts: {ATTEMPTS}")
 print_grid(board)
--- gentests.py
+++ gentests.py
@@ -11,6 +11,27 @@ def get_column(col, board):
 def get_row(row, board):
 	return board[row]
 
+def prefill(board, frame):
+	global FILLED_CELLS
+	for i in range(N):
+		if frame["top"][i] == 1:
+				board[0][i] = N
+		if frame["bottom"][i] == 1:
+				board[N-1][i] = N
+		if frame["left"][i] == 1:
+				board[i][0] = N
+		if frame["right"][i] == 1:
+				board[i][N-1] = N
+		for j in range(N):
+			if frame["top"][i] == N:
+				board[j][i] = j+1			
+			if frame["bottom"][i] == N:
+				board[N-1-j][i] = j+1
+			if frame["left"][i] == N:
+				board[i][j] = j+1
+			if frame["right"][i] == N:
+				board[i][N-1-j] = j+1
+
 def check_array(array, reverse):
 	last_seen = 0
 	counter_seen = 0
@@ -28,42 +49,74 @@ def can_place(row, col, board, value):
 	return True
 
 def	found_solution(board):
-	return all([all(board[x]) for x in range(N)])
+	global FOUND
+	FOUND = FILLED_CELLS == N*N
+	return FOUND
 
-def get_borders(board):
-	borders = {pos:[0 for x in range(N)] for pos in POSITIONS}
-	borders["left"] = [check_array(get_row(x, board), False) for x in range(N)]
-	borders["right"] = [check_array(get_row(x, board), True) for x in range(N)]
-	borders["top"] = [check_array(get_column(x, board), False) for x in range(N)]
-	borders["bottom"] = [check_array(get_column(x, board), True) for x in range(N)]
-	return borders
+def get_frame(board):
+	frame = {pos:[0 for x in range(N)] for pos in POSITIONS}
+	frame["left"] = [check_array(get_row(x, board), False) for x in range(N)]
+	frame["right"] = [check_array(get_row(x, board), True) for x in range(N)]
+	frame["top"] = [check_array(get_column(x, board), False) for x in range(N)]
+	frame["bottom"] = [check_array(get_column(x, board), True) for x in range(N)]
+	return frame
 	
 	
-def borders_to_str(borders):
-	return "".join("".join(f"{y} " for y in x) for x in list(borders.values()))[:-1]
+def frame_to_str(frame):
+	return "".join("".join(f"{y} " for y in x) for x in list(frame.values()))[:-1]
 
 def gentests(row, col, board):
+	global FILLED_CELLS
+	prefilled = False
 	if (found_solution(board)):
-		print(borders_to_str(get_borders(board)))
+		#print(frame_to_str(get_frame(board)))
+		frame = get_frame(board)
+		# values = [v for ar in list(frame.values()) for v in ar]
+		# if N in values:
+		# print(frame)
+		print(frame_to_str(frame))
 		print_grid(board)
 		return
-	choices = list(range(1, N+1))
-	random.shuffle(choices)
-	for choice in choices:
-		if (can_place(row, col, board, choice)):
-			board[row][col] = choice
-		else:
-			continue
+	if (board[row][col] != 0):
+		prefilled = True
 		next_col = (col + 1) % N
 		next_row = row
 		if (next_col == 0):
 			next_row = (row + 1) % N
 		gentests(next_row, next_col, board)
-	board[row][col] = 0
+	if not prefilled:	
+		choices = list(range(1, N+1))
+		random.shuffle(choices)
+		for choice in choices:
+			if (can_place(row, col, board, choice)):
+				board[row][col] = choice
+				FILLED_CELLS += 1
+			else:
+				continue
+			next_col = (col + 1) % N
+			next_row = row
+			if (next_col == 0):
+				next_row = (row + 1) % N
+			gentests(next_row, next_col, board)
+			board[row][col] = 0
+			FILLED_CELLS -= 1
 
+def count_filled(board):
+	global FILLED_CELLS
+	for row in board:
+		for cell in row:
+			if cell != 0:
+				FILLED_CELLS += 1
+
+FOUND = False
+FILLED_CELLS = 0
 N = int(sys.argv[1])
 POSITIONS = ["top", "bottom", "left", "right"]
 board = [[0 for x in range(N)] for x in range(N)]
-print(board)
+frame = sys.argv[2].split(" ")
+frame = {name:[int(frame[x*N + y]) for y in range(N)] for x, name in enumerate(POSITIONS)}
 
+prefill(board, frame)
+count_filled(board)
+print_grid(board)
 gentests(0, 0, board)
